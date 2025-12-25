292fd1254e2aecd05875d0a552ecf6a80adbfa6f
KaiRev0 <kairev0@outlook.com>
2025-12-25 23:31:26 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
+import os
 from flask import Flask
 from routes.base import base_bp
 from routes.auth import auth_bp
 from routes.client import client_bp
 from routes.cashier import cashier_bp
 from routes.admin import admin_bp
-from models.models import db, User
-from libs.libs import hash_password
+from models.models import db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fly.db'
+app = Flask(__name__) 
+
+database_url = os.environ.get('DATABASE_URL', 'sqlite:///fly.db')
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'super_secret'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'super_secret')
+
+db.init_app(app) 
 
 app.register_blueprint(base_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(client_bp)
 app.register_blueprint(cashier_bp)
 app.register_blueprint(admin_bp)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=False)
